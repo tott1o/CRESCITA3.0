3f1d226c18cae360fa60c5b2df247f43ca76f3dc
tott1o <akhilnathgs2006@gmail.com>
2026-01-11 19:39:12 +0530

    mob link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -87,7 +87,7 @@ const Navbar = () => {
                                 </a>
                             ))}
                             <div className="pt-2">
-                                <button className="w-full bg-blue-600 text-white py-3 rounded-lg font-bold">
+                                <button className="w-full bg-blue-600 text-white py-3 rounded-lg font-bold" onClick={() => window.open('https://bit.ly/CRESCITAEarlyBird', '_blank')}>
                                     Register
                                 </button>
                             </div>
