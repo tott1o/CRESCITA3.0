a6f63dabca24f73cc68dc4260b2f3b8bbb985c72
tott1o <akhilnathgs2006@gmail.com>
2026-01-20 09:30:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,6 @@ function App() {
   return (
     <div className="min-h-screen bg-slate-950 text-white font-sans selection:bg-blue-500/30">
       <Navbar />
-      <EarlyBirdPopup />
       <PromoPopup/>
       <main>
         <Routes>
